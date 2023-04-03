a5741b30031a275f06394fbf2147de5bce97f59e
NgHuuTrong <trongnh2003@gmail.com>
2023-04-03 14:17:28 +0700

    (#1) Install: nodemon, slugify, express

--- node-farm-project/index.js
+++ node-farm-project/index.js
@@ -1,6 +1,7 @@
 fs = require("fs");
 http = require("http");
 url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 ///////////// Server /////////////
@@ -19,6 +20,14 @@ const productsTemplate = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+const slug = slugify("nguyễn   hữu   trọng", {
+  locale: "vi",
+  trim: false,
+  replacement: " ",
+  strict: true,
+  remove: "n",
+});
+console.log(slug);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
