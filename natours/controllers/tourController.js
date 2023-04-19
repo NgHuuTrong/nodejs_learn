6a0526e248e3b424fdaec0481ca81488a1c5ac7d
NgHuuTrong <trongnh2003@gmail.com>
2023-04-19 16:06:32 +0700

    Chaining multiple middleware functions

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -4,6 +4,36 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log('Tour id is:', val);
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  const requestBody = req.body;
+  if (!requestBody.name || !requestBody.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: `Lack of ${
+        !requestBody.name
+          ? !requestBody.price
+            ? 'name and price'
+            : 'name'
+          : !requestBody.price
+          ? 'price'
+          : ''
+      }!`,
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -19,13 +49,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((ele) => ele.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -55,13 +78,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -71,13 +87,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
