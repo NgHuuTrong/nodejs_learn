438386ab655dbe98f974a33c7c654f5ba1e59e0b
NgHuuTrong <trongnh2003@gmail.com>
2023-04-22 17:03:21 +0700

    Import data to DB

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`));
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data imported successfully');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted successfully');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
