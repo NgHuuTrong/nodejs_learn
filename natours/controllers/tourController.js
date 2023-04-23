dc9acbd25fd724e0bd2bff82af5d5c8ef8efa430
NgHuuTrong <trongnh2003@gmail.com>
2023-04-23 15:04:41 +0700

    API: Sorting

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -24,17 +24,23 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) FILTERING
+    // 1A) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((ele) => delete queryObj[ele]);
 
-    // 2) ADVANCED FILTERING
+    // 1B) ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt'); // Sort the newest
+    }
 
     // EXECUTE QUERY
     const tours = await query;
