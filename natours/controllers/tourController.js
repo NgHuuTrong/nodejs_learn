4d36becae98c375b37dc4020009d678f6cd17b78
NgHuuTrong <trongnh2003@gmail.com>
2023-04-23 15:46:21 +0700

    API: Aliasing

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -21,6 +21,13 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = 5;
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
@@ -64,6 +71,7 @@ exports.getAllTours = async (req, res) => {
 
     // EXECUTE QUERY
     const tours = await query;
+    // query.sort().select().skip().limit()
 
     res.status(200).json({
       status: 'success',
