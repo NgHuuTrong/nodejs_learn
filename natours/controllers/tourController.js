39b57734abab2d2f1a993f76b136119038f77eea
NgHuuTrong <trongnh2003@gmail.com>
2023-04-23 15:34:59 +0700

    API: Pagination

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -42,12 +42,24 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt'); // Sort the newest
     }
 
-    // 2) FIELDS LIMITING
+    // 3) FIELDS LIMITING
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v'); // Sort the newest
+    }
+
+    // 4) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // EXECUTE QUERY
