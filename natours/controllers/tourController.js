eb0e903715aca8da2380078302ac94006af1f314
NgHuuTrong <trongnh2003@gmail.com>
2023-04-23 17:14:26 +0700

    Refractoring API Features

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,25 +1,6 @@
 // const fs = require('fs');
 const Tour = require('../models/tourModel');
-
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkBody = (req, res, next) => {
-//   const requestBody = req.body;
-//   if (!requestBody.name || !requestBody.price) {
-//     let lackDetail = '';
-//     if (!requestBody.name) {
-//       if (!requestBody.price) lackDetail = 'name and price';
-//       else lackDetail = 'name';
-//     } else if (!requestBody.price) lackDetail = 'price';
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: `Lack of ${lackDetail}!`,
-//     });
-//   }
-//   next();
-// };
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
@@ -30,48 +11,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((ele) => delete queryObj[ele]);
-
-    // 1B) ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt'); // Sort the newest
-    }
-
-    // 3) FIELDS LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v'); // Sort the newest
-    }
-
-    // 4) PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
-    // query.sort().select().skip().limit()
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
