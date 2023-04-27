3c1b30694e268263f4138815f84cdd0015fb6b75
NgHuuTrong <trongnh2003@gmail.com>
2023-04-27 13:45:28 +0700

    Global Error Handling Middleware

--- natours/app.js
+++ natours/app.js
@@ -12,11 +12,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -29,5 +25,29 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} in this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} in this server!`);
+  err.statusCode = 404;
+  err.status = 'fail';
+
+  next(err); // It will skip all other middleware in the stack and send the error that we passed in to our global error handling middleware
+});
+
+// Global handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
