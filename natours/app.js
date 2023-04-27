f0e520f0df3c721117fbbeae413b110341bf4605
NgHuuTrong <trongnh2003@gmail.com>
2023-04-27 14:07:20 +0700

    Refractor Global Error Handler

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -32,22 +34,16 @@ app.all('*', (req, res, next) => {
   //   message: `Can't find ${req.originalUrl} in this server!`,
   // });
 
-  const err = new Error(`Can't find ${req.originalUrl} in this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
+  // const err = new Error(`Can't find ${req.originalUrl} in this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
 
-  next(err); // It will skip all other middleware in the stack and send the error that we passed in to our global error handling middleware
+  // next(err); // It will skip all other middleware in the stack and send the error that we passed in to our global error handling middleware
+
+  next(new AppError(`Can't find ${req.originalUrl} in this server!`, 404)); // It will skip all other middleware in the stack and send the error that we passed in to our global error handling middleware
 });
 
 // Global handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
