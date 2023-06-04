fc48cc379ed2f6fb5efc2a3b82c9ab5a43c61cd9
NgHuuTrong <trongnh2003@gmail.com>
2023-06-04 16:48:55 +0700

    Sign-up Users (jwt)

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -36,10 +36,13 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
+  // Only run this function if the password was actually modified
   if (!this.isModified('password')) return next();
 
+  // Hash the password with cost of 12
   this.password = await bscypt.hash(this.password, 12);
 
+  // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
