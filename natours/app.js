982444176e510fc27bb9226ca67f30c1b47c218e
NgHuuTrong <trongnh2003@gmail.com>
2023-06-10 12:49:36 +0700

    Setting Security HTTP Headers

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. GLOBAL MIDDLEWARE
+
+// Set security HTTP headers
+app.use(helmet()); // use as the first of all middlewares
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,7 +28,9 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
