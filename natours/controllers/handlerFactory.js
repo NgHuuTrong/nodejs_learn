609675cf18effdeea1babec4a7484c1e3743a43b
NgHuuTrong <trongnh2003@gmail.com>
2023-06-21 23:53:06 +0700

    Factory Functions: Reading

--- natours/controllers/handlerFactory.js
+++ natours/controllers/handlerFactory.js
@@ -1,3 +1,4 @@
+const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
@@ -41,6 +42,46 @@ exports.createOne = (Model) =>
       status: 'success',
       data: {
         data: newDoc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    const query = Model.findById(req.params.id);
+    if (popOptions) query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested route GET reviews on tour (hack)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
       },
     });
   });
--- natours/controllers/reviewController.js
+++ natours/controllers/reviewController.js
@@ -1,27 +1,15 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
 exports.setTourUserIds = (req, res, next) => {
-  // Allow nested routes
+  // Allow nested routes (review on tour)
   if (!req.body.user) req.body.user = req.user.id;
   if (!req.body.tour) req.body.tour = req.params.tourId;
   next();
 };
+exports.getAllReviews = factory.getAll(Review);
+
+exports.getReview = factory.getOne(Review);
 
 exports.createReview = factory.createOne(Review);
 
