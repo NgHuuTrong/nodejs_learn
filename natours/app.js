e67e187562f4970a65e69712b07873a063f5f8e7
NgHuuTrong <trongnh2003@gmail.com>
2023-06-26 17:21:37 +0700

    Creating Base Template

--- natours/app.js
+++ natours/app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 
 // 3. ROUTES: import from external files
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'nghuutrong',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
